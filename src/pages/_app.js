4bf8e8b8f1567d906d8a91fb991d6e20cffc235a
Furkan Çakıcı <furkancakici25@gmail.com>
2023-03-09 00:17:34 +0300

    feat:nprogress add

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,12 @@
-import Layout from '@/containers/Layout';
-import '@/styles/globals.css';
+import NextNProgress from 'nextjs-progressbar'
+import Layout from '@/containers/Layout'
+import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  );
+    return (
+        <Layout>
+            <NextNProgress color='#FE595E' height={4} />
+            <Component {...pageProps} />
+        </Layout>
+    )
 }
